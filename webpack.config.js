310b52fc7cfe82aad85d845acb14a49e7e524d9b
wellington197 <fcowellcosta@gmail.com>
2022-04-13 10:39:09 -0300

    created build for  webpack.config.js in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path=require('path');
 
 module.exports={
-    
-}
+    entry: './src/index.js',
+    output:{
+        filename:'script.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    mode:'production'
+
+};
